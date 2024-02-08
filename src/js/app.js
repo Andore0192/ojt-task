a3b19635d137cc55a0536c37f4f72e5441a3ca91
Andore <eadsuva.student@ua.edu.ph>
2024-02-08 15:45:37 +0800

    fix image

--- src/js/app.js
+++ src/js/app.js
@@ -1,104 +1,116 @@
-// single comment use for code details or code guide for programmer
-/*  <---- opening for multi comment
-    multi comment use for mode code details or guides for programmer
- ----> closing for multi comment */
+// // single comment use for code details or code guide for programmer
+// /*  <---- opening for multi comment
+//     multi comment use for mode code details or guides for programmer//  ----> closing for multi comment */
 
-var variable ="Edward Andrei Suva";
+// var variable ="Edward Andrei Suva";
 
-// console.log("hello "+variable);
+// // console.log("hello "+variable);
 
-var baso_text = "seven pesos";
-var baso_number = 7;
+// var baso_text = "seven pesos";
+// var baso_number = 7;
 
-// let
-// const
-// console.log("text" +baso_text);
-// console.log("number "+baso_number);
+// // let
+// // const
+// // console.log("text" +baso_text);
+// // console.log("number "+baso_number);
 
-// console.log("text" ,baso_text);
-// console.log("number ",baso_number);
+// // console.log("text" ,baso_text);
+// // console.log("number ",baso_number);
 
-var first_text = "seven pesos";
-var second_text = "seven pesos";
-
+// var first_text = "seven pesos";
+// var second_text = "seven pesos";
 
 
 
-// console.log(first_number + second_text)
-// console.log(first_text + second_text)
-// console.log(first_number + second_number)
+
+// // console.log(first_number + second_text)
+// // console.log(first_text + second_text)
+// // console.log(first_number + second_number)
 
 
-// math OPERATORS
-// +
-// -
-// *
-// /
-// %
+// // math OPERATORS
+// // +
+// // -
+// // *
+// // /
+// // %
 
-// console.log("the sum of", first_number,"and ", second_number,"is", first_number + second_number) 
-// console.log("The difference of", first_number , "and", second_number, "is", first_number - second_number)
-// console.log("The product of",  first_number, "and",  second_number, "is", first_number * second_number) 
-// console.log("The dividend of", first_number, "and", second_number, "is", first_number / second_number)
-// console.log("The modulus of",first_number, "and", second_number, "is", first_number % second_number)
+// // console.log("the sum of", first_number,"and ", second_number,"is", first_number + second_number) 
+// // console.log("The difference of", first_number , "and", second_number, "is", first_number - second_number)
+// // console.log("The product of",  first_number, "and",  second_number, "is", first_number * second_number) 
+// // console.log("The dividend of", first_number, "and", second_number, "is", first_number / second_number)
+// // console.log("The modulus of",first_number, "and", second_number, "is", first_number % second_number)
 
-// conditional operators
-// == equal equal
-// != not equal
-// || or
-//  && and 
+// // conditional operators
+// // == equal equal
+// // != not equal
+// // || or
+// //  && and 
 
-// if(first_number == second_number){
-//     console.log("true")    
-// } else{
-//     console.log("false")
+// // if(first_number == second_number){
+// //     console.log("true")    
+// // } else{
+// //     console.log("false")
+// // }
+
+// // if(first_number != second_number){
+// //     console.log("true")    
+// // } else{
+// //     console.log("false")
+// // }
+
+// // var first_number = 7;
+// // var second_number = 8;
+
+// // if(first_number != second_number || first_number == second_number){
+// //     // ((7 != 8)  = true  or (7 == 8) = false )  = true
+// //     console.log("true")    
+// // } else{
+// //     console.log("false")
+// // }
+
+// // if(first_number != second_number && first_number == second_number){
+// //      // ((7 != 8)  = true  && (7 == 8) = false )  = false
+// //     console.log("true")    
+// // } else{
+// //     console.log("false")
+// // }
+
+// // var result = (first_number != second_number || first_number == second_number) ? true : false;
+// // console.log(result);
+
+
+// var player1 = "paper";
+// var player2 = "rock";
+
+
+
+// //
+// if (player1 == "rock" && player2 == "paper"){
+//     console.log(player2," won");
+// }
+// else if (player1 == "paper" && player2 == "rock"){
+//     console.log("player 1",player1," won");
+// }
+// else if(player2 =="paper" && player1 == "rock"){
+//     console.log("player 1",player1," won");
 // }
 
-// if(first_number != second_number){
-//     console.log("true")    
-// } else{
-//     console.log("false")
-// }
+// else if (player2 == "paper" && player1 == "rock"){
+//     console.log("player2" , player2, "won")
+// } 
 
-// var first_number = 7;
-// var second_number = 8;
-
-// if(first_number != second_number || first_number == second_number){
-//     // ((7 != 8)  = true  or (7 == 8) = false )  = true
-//     console.log("true")    
-// } else{
-//     console.log("false")
-// }
-
-// if(first_number != second_number && first_number == second_number){
-//      // ((7 != 8)  = true  && (7 == 8) = false )  = false
-//     console.log("true")    
-// } else{
-//     console.log("false")
-// }
-
-// var result = (first_number != second_number || first_number == second_number) ? true : false;
-// console.log(result);
-
-
-var player1 = "paper";
-var player2 = "rock";
-
-
-
-//
-if (player1 == "rock" && player2 == "paper"){
-    console.log(player2," won");
+function orderButton(){
+    var x = document.getElementById("btn-order");
+    var container = document.getElementById("order-container");
+    if (x.style.display === "none"){
+        console.log("test")
+        x.style.display = "block";
+        container.style.display ="none";
+    }
+    else {
+        x.style.display = "none";
+        container.style.display ="block";
+    }
 }
-else if (player1 == "paper" && player2 == "rock"){
-    console.log("player 1",player1," won");
-}
-else if(player2 =="paper" && player1 == "rock"){
-    console.log("player 1",player1," won");
-}
-
-else if (player2 == "paper" && player1 == "rock"){
-    console.log("player2" , player2, "won")
-} 
-
 
